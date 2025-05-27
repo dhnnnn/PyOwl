194bf3153b1b5505a8cd49000622db036f3a8b31
Dhani Dwi Putra <dhanidwiputra371@gmail.com>
2025-05-27 22:05:13 +0700

    Add files via upload

---
+++ Game owl hunter.py
@@ -0,0 +1,38 @@
+import random
+
+print("====================")
+print("=== PyOwl Hunter ===")
+print("====================\n")
+
+nama = input("Hai masukan nama kamu : ")
+
+while True: 
+    goa = ["|_|", "|_|", "|_|", "|_|"]
+
+    posisi = random.randint(1, 4)
+
+    goa[posisi - 1] = "|ʘ̅͜ʘ̅|"
+
+    goa = " ".join(goa)
+
+
+    print("\nPerhatikan goa di bawah ini : \n\n|_|", "|_|", "|_|", "|_|\n")
+    pilih = int(input(f"Hai {nama}, tebak dimanakah owl bersarang [1/2/3/4] : "))
+
+    if pilih == posisi :
+        print(f"\n{goa}\n\nSelamat kamu menang")
+    elif pilih < 0 or pilih > 4 :
+        print("\nPilihan kamu tidak ada")
+    else:
+        print(f"\n{goa}\n\nHAHAHA kamu kalah")
+
+    mulai_ulang = input("\n\nApakah kamu ingin mengulangi gamenya [y/n] : ")
+    
+    if mulai_ulang == "y":
+        continue
+    elif mulai_ulang == "n":
+        print(f"\nTerimakasih {nama} telah menggunakan program kami, progam akan di hentikan")
+        break        
+    else:
+        print("\nPilih dengan benar dong, progam akan dihentikan")
+        break
